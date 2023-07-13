a1986e1e1f73c356e21826283ebdaccc9db8c994
Ali Hassan Sharif <71347625+AliSharif12354@users.noreply.github.com>
2023-07-13 17:52:09 -0400

    Update SearchBar.js

--- screens/SearchBar.js
+++ screens/SearchBar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 
 const SearchBar = ({ onSearch }) => {
   const [searchText, setSearchText] = React.useState('');
@@ -10,16 +11,31 @@ const SearchBar = ({ onSearch }) => {
 
   return (
     <View style={styles.container}>
-      <TextInput
-        style={styles.input}
-        placeholder="Enter your search"
-        value={searchText}
-        onChangeText={setSearchText}
-        onSubmitEditing={handleSearch}
-      />
-      <TouchableOpacity style={styles.button} onPress={handleSearch}>
-        <Text style={styles.buttonText}>Search</Text>
-      </TouchableOpacity>
+      <Text style={styles.title}>THRIFTY</Text>
+      <View style={styles.buttonsContainer}>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>Sign Up</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>Login</Text>
+          </TouchableOpacity>
+      </View>
+      <View style={styles.contentContainer}>
+        <View style={styles.searchBarContainer}>
+          <TextInput
+            style={styles.input}
+            placeholder="Enter your search"
+            value={searchText}
+            onChangeText={setSearchText}
+            onSubmitEditing={handleSearch}
+          />
+          <TouchableOpacity style={styles.searchButton} onPress={handleSearch}>
+            <Ionicons name="search" size={24} color="#fff" />
+          </TouchableOpacity>
+        </View>
+
+      </View>
     </View>
   );
 };
@@ -29,23 +45,48 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#202020',
+    backgroundColor: '#000',
   },
-  input: {
+  title: {
+    fontSize: 32,
+    color: '#fff',
+    fontFamily: 'Helvetica',
+    marginBottom: 16,
+  },
+  contentContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  searchBarContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
     width: '80%',
     height: 40,
     borderWidth: 1,
     borderColor: '#ccc',
     borderRadius: 8,
+    marginBottom: 500,
     paddingHorizontal: 10,
-    marginBottom: 16,
+  },
+  input: {
+    flex: 1,
     color: '#fff',
+  },
+  searchButton: {
+    padding: 8,
+  },
+  buttonsContainer: {
+    alignItems: 'center',
+    flexDirection: 'row',
+    
   },
   button: {
     backgroundColor: '#007AFF',
     paddingVertical: 10,
     paddingHorizontal: 20,
     borderRadius: 8,
+    marginRight: 10,
   },
   buttonText: {
     color: '#fff',
